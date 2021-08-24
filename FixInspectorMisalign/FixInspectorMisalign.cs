edf4784084af7370d225254ff81effe1d69b2153
art0007i <art0007i@gmail.com>
2021-08-25 00:21:44 +0200

    Added actual code

--- FixInspectorMisalign/FixInspectorMisalign.cs
+++ FixInspectorMisalign/FixInspectorMisalign.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using FrooxEngine;
 using FrooxEngine.LogiX;
+using FrooxEngine.UIX;
+using BaseX;
+using System.Reflection.Emit;
 
 namespace FixInspectorMisalign
 {
@@ -22,12 +25,60 @@ namespace FixInspectorMisalign
             harmony.PatchAll();
 
         }
-        [HarmonyPatch(typeof(LogixHelper), "GetImpulseTargets")]
-        class TemplatePatch
+
+        [HarmonyPatch(typeof(PrimitiveMemberEditor))]
+        [HarmonyPatch("BuildUI")]
+        class PrimitiveMemberEditor_BuildUI_Patch
         {
-            public static bool Prefix()
+            public static void Postfix(UIBuilder ui)
             {
-                return false;
+                ui.Style.MinWidth = -1f;
+            }
+        }
+        [HarmonyPatch(typeof(QuaternionMemberEditor))]
+        [HarmonyPatch("BuildUI")]
+        class QuaternionMemberEditor_BuildUI_Patch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var foundIndex = -1;
+
+                var codes = new List<CodeInstruction>(instructions);
+
+                var alignConstructor = typeof(Alignment?).GetConstructor(new Type[] { typeof(Alignment) });
+                var textMethod = typeof(UIBuilder).GetMethod("Text", new Type[] { typeof(FrooxEngine.LocaleString), typeof(Boolean), typeof(Alignment?), typeof(Boolean), typeof(String) });
+
+                for (var i = 0; i < codes.Count; i++)
+                {
+                    //this checks if its an null algnment field, and saves the index for later
+                    if (codes[i].opcode == OpCodes.Initobj && codes[i].operand == typeof(Alignment?) && codes[i - 1].opcode == OpCodes.Ldloca_S)
+                    {
+                        foundIndex = i;
+                    }
+                    //this looks for the text creation method, we want them to align to leftmiddle (3 as int)
+                    if (codes[i].opcode == OpCodes.Callvirt && codes[i].operand is MethodInfo && (codes[i].operand as MethodInfo).Name == "Text" && foundIndex != -1)
+                    {
+                        //this edits stuff around the saved index to change alignment from null to leftmiddle
+                        codes[foundIndex].opcode = OpCodes.Newobj;
+                        codes[foundIndex].operand = alignConstructor;
+                        codes[foundIndex - 1].opcode = OpCodes.Ldc_I4_3;
+                        codes[foundIndex - 1].operand = null;
+                        //should i be removing from the instructions list?
+                        //for now i just do nops cause then i dont have to account for index shifts
+                        codes[foundIndex + 1].opcode = OpCodes.Nop;
+                        codes[foundIndex + 1].operand = null;
+                        foundIndex = -1;
+                    }
+                }
+
+                //This prints the il code, useful for debugging
+                /*
+                for(var i = 0; i < codes.Count; i++)
+                {
+                    UniLog.Log("IL_"+i.ToString("0000") + ": " + codes[i].ToString());
+                }
+                */
+                return codes.AsEnumerable();
             }
         }
     }
